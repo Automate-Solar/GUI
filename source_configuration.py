3390130548ae518bec29916f6305300fa997fd2d
jonscragg <jonathan.scragg@uu.se>
2024-05-27 12:54:19 +0200

    updates

---
+++ source_configuration.py
@@ -0,0 +1,16 @@
+# position in array = source, value in array = supply
+
+sources = [1, 2, 3, 4, 5, 6]
+qcms = [1, 1, 2, 2, 3, 3] #which qcm is nearest each source
+
+supplies = [1, 2, 3, 4, 5, 6] #which PS is connect to which source.
+materials = ["Zr", "CuS", "Sn", "SnS", "Ba", "ZnS"]
+targets = ["Zr_01", "CuS_01", "Sn_01", "SnS_01", "Ba_01", "ZnS_01"]
+modes = ["pulsed DC", "pulsed DC", "pulsed DC", "pulsed DC", "RF", "RF"]
+max_powers = [70, 50, 50, 50, 70, 50]
+ramp_rates = [10, 5, 10, 5, 5, 5]
+presputter_powers = [50, 35, 35, 35, 50, 35]
+presputter_voltages = [0, 0, 0, 0, 0, 0]
+
+
+
